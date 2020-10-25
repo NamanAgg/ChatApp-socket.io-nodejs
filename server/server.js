5dd56af5b23443a1665e1184a6b21236c0c2ea0e
naman <namanagg.2010@gmail.com>
2020-10-26 03:23:42 +0530

    add connect and disconnect event listener using socket.io

--- server/server.js
+++ server/server.js
@@ -1,30 +1,41 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 //const request= require("request");
 const ejs = require("ejs");
-const https=require("https");
+const http=require("http");
 const _=require("lodash");
 const mongoose=require("mongoose");
 const path=require("path");
 const app = express();
 const socketIO=require("socket.io");
 
-const port=process.env.PORT|| 3000
+const port=process.env.PORT|| 3000;
+
+
 app.set('view engine', 'ejs');
 //mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-let server=https.createServer(app);
-
+let server=http.createServer(app);
+let io= socketIO(server);
 app.get("/",function(req,res){
   //res.sendFile(path.resolve('../public/blah.html'));
-  //res.render("index");
+  res.render("index");
+});
 
+io.on('connection',(socket)=>{
+  console.log("A new user just got connected");
 
+  socket.on("disconnect",()=>{
+    console.log("User was disconnected");
+  });
 });
 
 
-app.listen(port, function() {
+
+
+server.listen(port,()=>{
   console.log('server started on port '+ port);
 
 });
